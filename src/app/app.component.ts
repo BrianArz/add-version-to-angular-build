8b99a1df0db7aaa792851e3852fa44d31e45a352
Rens Jaspers <rens.jaspers@gmail.com>
2024-01-05 14:39:12 +0100

    feat: add version info to build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { version, commitHash, buildDate } from '../environments/version';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'buildversion';
+  versionInfo = { version, commitHash, buildDate };
+  constructor() {
+    console.log(this.versionInfo);
+  }
 }
